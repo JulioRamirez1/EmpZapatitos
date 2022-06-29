61bc77e0f6185fc7eb1d21057fab08baad27b6ed
JulioRamirez1 <jarsanchez01@gmail.com>
2022-06-29 12:55:21 -0500

    Correccion del metodo precio

--- src/poo02_zapatitos/Zapato.java
+++ src/poo02_zapatitos/Zapato.java
@@ -18,6 +18,7 @@ public class Zapato {
 
     public void setTalla(Integer talla) {
         this.talla = talla;
+        this.calcularPrecio();
     }
 
     public Double getPrecio() {
@@ -39,8 +40,23 @@ public class Zapato {
     }
     
     private void calcularPrecio() {
-        this.precio = 0;
-        if ("Ejecutivo".equals(this.modelo) && this.talla >= 8) {
+        this.precio = 0; 
+        var aumento = (this.talla == 8 ? 0 : this.talla == 9 ? 1 : 2) * 10;
+        
+        switch (this.modelo) {
+            case "Ejecutivo":
+                this.precio = 345.50 + aumento;  
+                break;
+            
+            case "Premier":
+                this.precio = 298.70 + aumento;  
+                break;
+            
+            case "Emperador":
+                this.precio = 246.00 + aumento;  
+        }
+                
+        /*if ("Ejecutivo".equals(this.modelo) && this.talla >= 8) {
             this.precio = 345.50 + 10 * (this.talla = 8);
         }
         
@@ -50,7 +66,7 @@ public class Zapato {
         
         if ("Emperador".equals(this.modelo) && this.talla >= 8) {
             this.precio = 246.00 + 10 * (this.talla = 8);
-        }
+        }*/
     }
 
     @Override
